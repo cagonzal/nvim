817e924353b5d52e4e1e192a59adf8556cc74638
cagonzal <gonzalez.a.carlos09@gmail.com>
2024-08-29 13:02:03 -0700

    Added lsp config and telescope pop-up ui

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,9 @@
-return { 
-        'nvim-telescope/telescope.nvim', 
+return {
+
+    {
+        'nvim-telescope/telescope.nvim',
         branch = '0.1.x',
-        dependencies = { 
+        dependencies = {
         'nvim-lua/plenary.nvim',
         {"nvim-telescope/telescope-fzf-native.nvim", build="make"},
         "nvim-tree/nvim-web-devicons",
@@ -37,4 +39,19 @@ return {
             --vim.keymap.set('n', '<C-p>', builtin.find_files, {})
             --vim.keymap.set('n','<leader>fg', builtin.live_grep, {})
         end,
+    },
+    {
+       "nvim-telescope/telescope-ui-select.nvim",
+       config = function()
+           require("telescope").setup({
+               extensions = {
+                   ["ui-select"] = {
+                       require("telescope.themes").get_dropdown {
+                       }
+                   }
+               }
+           })
+           require("telescope").load_extension("ui-select")
+       end
+    },
 }
