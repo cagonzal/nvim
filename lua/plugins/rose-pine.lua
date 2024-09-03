d64e4fe97b749a14eb01bae5a506a8a7b3d70c67
cagonzal <gonzalez.a.carlos09@gmail.com>
2024-09-03 14:37:35 -0700

    Added more features to rose-pine file

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -3,10 +3,48 @@ return {
     name = "rose-pine",
     config = function()
         return {
+            variant = "auto",
+            dark_variant = "main",
+            extend_background_behind_borders = true,
+
             styles = {
-                transparency = true,
+                bold = true,
+                italic = true,
+                transparency = false,
             },
-            vim.cmd.colorscheme "rose-pine"
+
+            groups = {
+                border = "muted",
+                link = "iris",
+                panel = "surface",
+
+                error = "love",
+                hint = "iris",
+                info = "foam",
+                note = "pine",
+                todo = "rose",
+                warn = "gold",
+
+                git_add = "foam",
+                git_change = "rose",
+                git_delete = "love",
+                git_dirty = "rose",
+                git_ignore = "muted",
+                git_merge = "iris",
+                git_rename = "pine",
+                git_stage = "iris",
+                git_text = "rose",
+                git_untracked = "subtle",
+
+                h1 = "iris",
+                h2 = "foam",
+                h3 = "rose",
+                h4 = "gold",
+                h5 = "pine",
+                h6 = "foam",
+            },
+
+            vim.cmd.colorscheme "rose-pine-moon"
         }
     end,
 }
