d054ecc58bc6526b579d8f34527d676a9efe31b2
cagonzal <gonzalez.a.carlos09@gmail.com>
2024-10-19 11:41:56 -0700

    added vimtex and lsp autocomplete

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,20 +9,30 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "jedi_language_server", }
+                ensure_installed = { "lua_ls", "jedi_language_server", "ltex"}
             })
         end
     },
     {
         "neovim/nvim-lspconfig",
         config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            -- import lspconfig plugin
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.jedi_language_server.setup({})
 
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.jedi_language_server.setup({
+                capabilities = capabilities
+            })
+            lspconfig.ltex.setup({
+                capabilities = capabilities
+            })
         end
     }
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -19,6 +19,11 @@ vim.keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
 vim.keymap.set("n", "<leader>se", "<C-w>=") -- sequalize windows
 vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>") -- close window 
 
+vim.keymap.set("n", "<leader>j", ":resize +2<cr>")
+vim.keymap.set("n", "<leader>k", ":resize -2<cr>")
+vim.keymap.set("n", "<leader>l", ":vertical resize +2<cr>")
+vim.keymap.set("n", "<leader>h", ":vertical resize -2<cr>")
+
 vim.keymap.set("n", "<leader>bn", "<cmd>bnext<CR>")
 vim.keymap.set("n", "<leader>bp", "<cmd>bprev<CR>")
 vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<CR>")
@@ -26,13 +31,28 @@ vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<CR>")
 --vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>") -- close current tab
 --vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>") -- go to next tab
 --vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>") -- go to prev tab
---vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>") -- open current buffer in a new tab
+--vim.keymap.set("n", "<leader>tf", "<cmd>tab new %<CR>") -- open current buffer in a new tab
 -- Navigation
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
+vim.keymap.set("n", "<leader>sr", ":%s/<<C-r><C-w>>//g<Left><Left>")
+
 vim.opt.mouse = ""
 
 vim.g.nightflyCursorColor = true
+
+
+function SpellToggle()
+    if vim.opt.spell:get() then
+        vim.opt_local.spell = false
+        vim.opt_local.spelllang = "en"
+    else
+        vim.opt_local.spell = true
+        vim.opt_local.spelllang = {"en_us"}
+    end
+end
+vim.keymap.set("n", "<leader>5", ":lua SpellToggle()<cr>")
+vim.keymap.set("n", "<leader>z", "[s1z=``")
