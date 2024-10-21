a76e0882f7c179fcd71bf1e827f680aa4d4271ce
cagonzal <gonzalez.a.carlos09@gmail.com>
2024-10-21 11:17:56 -0700

    added more tsd for parens

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -6,5 +6,18 @@ return {
     -- VimTeX configuration goes here, e.g.
     vim.g.vimtex_view_method = "zathura"
     vim.g.vimtex_quickfix_open_on_warning = 0
+    vim.g.tex_flavor = "latex"
+    vim.g.vimtex_log_ignore = ({
+        "Underfull",
+        "Overfull",
+        "specifier changed to",
+        "Token not allowed in a PDF string",
+    })
+    vim.g.vimtex_delim_toggle_mod_list = ({
+        {'\\left', '\\right'},
+        {'\\bigl', '\\bigr'},
+        {'\\biggl', '\\biggr'}
+    })
+
   end
 }
