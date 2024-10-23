7b27aaf48d57da621c22559f199f027ba542401b
cagonzal <gonzalez.a.carlos09@gmail.com>
2024-10-22 21:51:20 -0700

    using sioyek for pdfs and added vim surround

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -4,7 +4,7 @@ return {
   -- tag = "v2.15", -- uncomment to pin to a specific release
   init = function()
     -- VimTeX configuration goes here, e.g.
-    vim.g.vimtex_view_method = "zathura"
+    vim.g.vimtex_view_method = "sioyek"
     vim.g.vimtex_quickfix_open_on_warning = 0
     vim.g.tex_flavor = "latex"
     vim.g.vimtex_log_ignore = ({
